c5e46a28f163795fb790aeb2dd9d51edb5bb3035
Brian Webb <bwebb@indatus.com>
2013-07-01 15:44:13 -0400

    debugging xdebug install

--- recipes/default.rb
+++ recipes/default.rb
@@ -61,7 +61,7 @@ bash "install_xdebug" do
     wget http://xdebug.org/files/xdebug-#{node[:php][:xdebug_version]}.tgz -O /usr/src/xdebug-#{node[:php][:xdebug_version]}.tgz
     tar -zxf xdebug-#{node[:php][:xdebug_version]}.tgz
     (cd xdebug-#{node[:php][:xdebug_version]} && phpize && ./configure && make)
-    (cd /usr/src/xdebug-#{node[:php][:xdebug_version]} && phpize | grep "Zend\ Module" | sed 's/[^0-9.]*\([0-9.]*\).*/\1/' > /tmp/phpize_version.txt)
+    (cd /usr/src/xdebug-#{node[:php][:xdebug_version]} && phpize | grep "Zend\ Module" | sed 's/[^0-9]*//g' > /tmp/phpize_version.txt)
     (cp modules/xdebug.so /usr/lib/php5/`cat /tmp/phpize_version.txt`/)    
   EOH
 end
